43228fa4afe6528e97db0f95666b7495861e12ab
Geismar-Maintenance <damon.tinsley@mauserpackaging.com>
2026-04-08 14:45:31 -0500

    Update issue.js

--- api/parts/issue.js
+++ api/parts/issue.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
+  console.log("ISSUE REQUEST BODY:", req.body);
   try {
     const client = await pool.connect();
 
