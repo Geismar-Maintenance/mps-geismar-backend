9c4dbe9218ab221ee006694445b04d180ffa7201
Geismar-Maintenance <damon.tinsley@mauserpackaging.com>
2026-04-17 10:52:15 -0500

    Update pm.js

--- api/pm.js
+++ api/pm.js
@@ -8,7 +8,7 @@ const pool = new Pool({
 });
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://geismar-maintenance.github.io");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
