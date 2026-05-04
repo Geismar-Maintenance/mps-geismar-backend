f3e42b126e77579fd11d478373704de038be9408
Geismar-Maintenance <damon.tinsley@mauserpackaging.com>
2026-05-04 14:35:30 -0500

    Allow POST method in CORS headers

--- api/admin.js
+++ api/admin.js
@@ -12,7 +12,7 @@ const pool = new Pool({
 export default async function handler(req, res) {
   // ✅ CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
