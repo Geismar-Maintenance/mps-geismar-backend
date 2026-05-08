19ca5bac199bfbb48bfd73c05a55ab48cecd3621
Geismar-Maintenance <damon.tinsley@mauserpackaging.com>
2026-05-08 08:31:02 -0500

    Change section filter to use LIKE operator

--- api/reports.js
+++ api/reports.js
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
         JOIN masterparts p ON p.partid = pl.partid
         JOIN locations l ON l.locationid = pl.locationid
         WHERE l.cabinet = $1
-          AND l.section = $2
+          AND l.section LIKE $2
         ORDER BY l.bin, p.partnumber
         `,
         [cabinet, section]
